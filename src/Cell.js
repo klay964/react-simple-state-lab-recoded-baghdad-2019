cff68c79e47d85f40a209581b2cb2e02b9f3125b
Mohammed Ali <dody200039@gmail.com>
2020-01-19 06:08:59 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 
 export default class Cell extends Component {
   
-  constructor(props) {
-    super()
-    this.state = {
-      color: props.value
-    }
-  }
+
   
   handleClick = () => {
     this.setState({
